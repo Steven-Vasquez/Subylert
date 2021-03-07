23a336ac77a0ca04fc1caed6244f482f3bfa5714
adam-shafi <adam0shafi@gmail.com>
2021-03-07 13:04:41 -0600

    partially merged?

---
+++ AndroidStudioProjects/SubmarineManagerGit/SubscriptionManager/app/src/main/java/com/example/subscriptionmanager/AddSubscriptionActivity.java
@@ -0,0 +1,231 @@
+package com.example.subscriptionmanager;
+
+import android.app.DatePickerDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+import android.view.View;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+public class AddSubscriptionActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    Calendar myCalendar = Calendar.getInstance();
+    private EditText edittext;
+    String[] courses = {"Day(s)", "Week(s)", "Month(s)", "Year(s)"};
+    EditText subName;
+    EditText subPrice;
+    EditText subFrequency;
+    EditText subCalendar;
+    Button btSave;
+    private int startMonth;
+    private int startDate;
+    private int startYear;
+
+    private String format1="MM";
+    private String format2="0000";
+
+
+    List<MainData> dataList = new ArrayList<>();
+    RoomDB database;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_subscription);
+        setUpDatePicker();
+// Take the instance of Spinner and
+        // apply OnItemSelectedListener on it which
+        // tells which item of spinner is clicked
+        Spinner spin = findViewById(R.id.spinner);
+       spin.setOnItemSelectedListener(this);
+        // Create the instance of ArrayAdapter
+        // having the list of courses
+        ArrayAdapter ad = new ArrayAdapter(this, android.R.layout.simple_spinner_item, courses);
+        // set simple layout resource file
+        // for each item of spinner
+//        ad.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Set the ArrayAdapter (ad) data on the
+        // Spinner which binds data to spinner
+        spin.setAdapter(ad);
+
+        subName = findViewById(R.id.subscriptionName);
+        subPrice = findViewById(R.id.cost);
+        subFrequency = findViewById(R.id.frequency);
+
+        btSave = findViewById(R.id.save_add_subscription);
+
+
+        //Initialize database
+        database = RoomDB.getInstance(this);
+        //Store database value in data list
+        dataList = database.mainDao().getAll();
+
+        //Initialize linear layout manager
+
+
+    }
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        MenuInflater inflater = getMenuInflater();
+//        inflater.inflate(R.menu.period_menu, menu);
+//        return true;
+//    }
+
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle item selection
+//        switch (item.getItemId()) {
+//            case R.id.periodMenu:
+//                newGame();
+//                return true;
+//            case R.id.help:
+//                showHelp();
+//                return true;
+//            default:
+//                return super.onOptionsItemSelected(item);
+//        }
+//    }
+
+    // Performing action when ItemSelected
+    // from spinner, Overriding onItemSelected method
+    @Override
+    public void onItemSelected(AdapterView<?> arg0, View arg1, int position, long id) {
+        // make toast of name of course
+        // which is selected in spinner
+        Toast.makeText(getApplicationContext(),
+                courses[position],
+                Toast.LENGTH_LONG)
+                .show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> arg0) {
+        // Auto-generated method stub
+    }
+
+    public void onSave(View v) {
+        Spinner spin = findViewById(R.id.spinner);
+        if(!subName.getText().equals("") &&!subPrice.getText().toString().equals("") &&!subFrequency.getText().toString().equals("") &&myCalendar.getTime()!=null) {
+            String sName = subName.getText().toString().trim();
+            double sPrice = Double.parseDouble(subPrice.getText().toString().trim());
+            int sFrequency = Integer.parseInt(subFrequency.getText().toString().trim());
+            String sFrequencyType = spin.getSelectedItem().toString();
+            Date sStartDate = myCalendar.getTime();
+
+            //Check condition
+
+                //When text is not empty
+                //Initialize main data
+                MainData data = new MainData();
+                //Set text on main data
+                data.setText(sName);
+                data.setPrice(sPrice);
+                data.setFrequency(sFrequency);
+                data.setFrequencyType(sFrequencyType);
+            data.setDate(sStartDate);
+//                data.setStartMonth(startMonth);
+//                data.setStartDate(startDate);
+//                data.setStartYear(startYear);
+                Log.d("AddSubscriptionActivity","month:" + (data.getStartMonth())+" date: " + data.getStartDate()+ " year: "+data.getStartYear());
+
+                //Insert text in database
+                database.mainDao().insert(data);
+                //clear edit text
+                subName.setText("");
+                subPrice.setText("");
+                subFrequency.setText("");
+
+                //Notify when data is inserted
+                dataList.clear();
+                dataList.addAll(database.mainDao().getAll());
+
+
+
+        }
+        Intent intent = new Intent(this, MainAppActivity.class);
+        this.startActivity(intent);
+    }
+
+
+    public void setUpDatePicker() {
+        edittext = (EditText) findViewById(R.id.starting_date);
+        DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                // TODO Auto-generated method stub
+//                startMonth = monthOfYear+1;
+//                startDate = dayOfMonth;
+//                startYear = year;
+               myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                //myCalendar.set(year, monthOfYear, dayOfMonth);
+                updateLabel();
+            }
+
+        };
+
+        edittext.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                new DatePickerDialog(AddSubscriptionActivity.this, date, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
+
+    private void updateLabel() {
+        String myFormat = "MM/DD/YYYY"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+        String test = format1.format(Integer.toString(myCalendar.get(Calendar.MONTH)+1))+"/"
+                +format1.format(Integer.toString(myCalendar.get(Calendar.DATE)))+"/"
+                +format2.format(Integer.toString(myCalendar.get(Calendar.YEAR)));
+        Log.d("MainActivity", test);
+        //edittext.setText(sdf.format(myCalendar.getTime()));
+
+        edittext.setText(
+                test);
+    }
+
+}
---
+++ AndroidStudioProjects/SubmarineManagerGit/SubscriptionManager/app/src/main/java/com/example/subscriptionmanager/MainAdapter.java
@@ -0,0 +1,174 @@
+package com.example.subscriptionmanager;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.text.DecimalFormat;
+import java.util.List;
+
+public class MainAdapter extends RecyclerView.Adapter<MainAdapter.ViewHolder> {
+    //Initialize variable
+    private List<com.example.subscriptionmanager.MainData> dataList;
+    private Activity context;
+    private RoomDB database;
+    private final DecimalFormat money = new DecimalFormat("$0.00");
+    //Create constructor
+    public MainAdapter(Activity context, List<com.example.subscriptionmanager.MainData> dataList) {
+        this.context = context;
+        this.dataList = dataList;
+        notifyDataSetChanged();
+
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        //Initialize view
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.list_row_expandable, parent, false);
+        return new ViewHolder(view);
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final ViewHolder holder, int position) {
+        //Initialize main data
+
+        com.example.subscriptionmanager.MainData data = dataList.get(position);
+        //Initialize database
+        database = RoomDB.getInstance(context);
+
+        TextView textView;
+        TextView priceView;
+        TextView frequencyView;
+
+        //Set text on text view
+        //error
+        holder.textView.setText(data.getText());
+        holder.priceView.setText(money.format(data.getPrice()));
+        holder.frequencyView.setText(Integer.toString(data.getFrequency()));
+        holder.frequencyTypeView.setText(data.getFrequencyType());
+        holder.startDateView.setText("start date: " +data.getStartMonth()+"/"+data.getStartDate()+"/"+"2021");
+        Log.d("MainAdapter","start date: " +data.getStartMonth()+"/"+data.getStartDate()+"/"+data.getStartYear());
+
+        holder.btEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Initialize main data
+                com.example.subscriptionmanager.MainData d = dataList.get(holder.getAdapterPosition());
+                //Get id
+                final int sID = d.getID();
+                //Get text/other info
+                String sText = d.getText();
+                double sPrice = d.getPrice();
+                int sFrequency = d.getFrequency();
+
+                //Create dialog
+                final Dialog dialog = new Dialog(context);
+                //Set content view
+                dialog.setContentView(R.layout.activity_add_subscription);
+                //Initialize width
+                int width = WindowManager.LayoutParams.MATCH_PARENT;
+                //Initialize height
+                int height = WindowManager.LayoutParams.WRAP_CONTENT;
+                //Set layout
+                dialog.getWindow().setLayout(width, height);
+                //Show dialog
+                dialog.show();
+
+                //Initialize and assign variable
+                final EditText editText = dialog.findViewById(R.id.subscriptionName);
+                final EditText editPrice = dialog.findViewById(R.id.cost);
+                final EditText editFrequency = dialog.findViewById(R.id.frequency);
+
+                Button btUpdate = dialog.findViewById(R.id.save_add_subscription);
+
+
+                //Set text on edit text
+                editText.setText(sText);
+                editPrice.setText(Double.toString(sPrice));
+                editFrequency.setText(Integer.toString(sFrequency));
+
+
+                btUpdate.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        //Dismiss dialog
+                        dialog.dismiss();
+                        
+                        //Get updated text from edit text
+                        String uText = editText.getText().toString().trim();
+                        double uPrice = Double.parseDouble(editPrice.getText().toString().trim());
+                        int uFrequency = Integer.parseInt(editFrequency.getText().toString().trim());
+
+                        //Update text in database
+                        database.mainDao().update(sID, uText, uPrice, uFrequency);
+                        //Notify when data is updated
+                        dataList.clear();
+                        dataList.addAll(database.mainDao().getAll());
+                        notifyDataSetChanged();
+                    }
+                });
+            }
+        });
+
+
+
+        holder.btDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Initialize main data
+                com.example.subscriptionmanager.MainData d = dataList.get(holder.getAdapterPosition());
+                //Delete text from database
+                database.mainDao().delete(d);
+                //Notify when data is deleted
+                int position = holder.getAdapterPosition();
+                dataList.remove(position);
+                notifyItemRemoved(position);
+                notifyItemRangeChanged(position, dataList.size());
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return dataList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        //Initialize variable
+        TextView textView;
+        TextView priceView;
+        TextView frequencyView;
+        TextView frequencyTypeView;
+        TextView startDateView;
+
+        ImageView btDelete;
+        Button btEdit;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            //Assign variable
+            textView = itemView.findViewById(R.id.text_view);
+            priceView = itemView.findViewById(R.id.price_view);
+            frequencyView = itemView.findViewById(R.id.frequency_view);
+            frequencyTypeView = itemView.findViewById(R.id.frequency_type_view);
+            startDateView = itemView.findViewById(R.id.start_date_view);
+
+            btEdit = itemView.findViewById(R.id.bt_edit);
+            btDelete = itemView.findViewById(R.id.bt_delete);
+        }
+    }
+}
