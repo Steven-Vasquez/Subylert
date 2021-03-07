9d87ddeb943a21c6b79d151f9d28804808927916
adam-shafi <adam0shafi@gmail.com>
2021-03-06 19:31:18 -0600

    Google Sign in and sign out UI

--- app/src/main/java/com/example/subscriptionmanager/MainAppActivity.java
+++ app/src/main/java/com/example/subscriptionmanager/MainAppActivity.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.widget.ExpandableListView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.text.DecimalFormat;
@@ -16,6 +17,9 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class MainAppActivity extends AppCompatActivity {
     DecimalFormat money = new DecimalFormat("$0.00");
@@ -25,8 +29,42 @@ public class MainAppActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_app);
+        findViewById(R.id.signOutButton).setOnClickListener(new View.OnClickListener() {
 
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()) {
+                    case R.id.signOutButton:
+                        signOut();
+                        break;
+                }
+            }
+        });
     }
+    private void signOut() {
+
+        MainActivity.mGoogleSignInClient.signOut()
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        closeSubscription();
+                        //revokeAccess();
+
+                    }
+                });
+    }
+
+    //Later, come back and follow these instructions at bottom of page to remove google account info from app
+    //https://developers.google.com/identity/sign-in/android/disconnect
+    /*private void revokeAccess() {
+        MainActivity.mGoogleSignInClient.revokeAccess()
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        // ...
+                    }
+                });
+    }*/
     @Override
     protected void onStart() {
         super.onStart();
@@ -40,6 +78,11 @@ public class MainAppActivity extends AppCompatActivity {
     }
     public void addNewSubscription(View v) {
         Intent intent = new Intent(this, AddSubscriptionActivity.class);
+        this.startActivity(intent);
+    }
+    public void closeSubscription() {
+        Intent intent = new Intent(this, MainActivity.class);
+        this.finish();
         this.startActivity(intent);
     }
 }
